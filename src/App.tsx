60a4f236e20171960f5876e24ae6dfa701c9cb37
vicentepaiva <vicentepaivadev@gmail.com>
2022-02-13 17:03:17 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import {GlobalStyle} from './assets/styles/global'
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
-function App() {
+import {  TransactionsProvider, useTransactions } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+  
+
+  function handleOpenNewTrasactionModal() {
+      setIsNewTrasactionModalOpen(true);
+  }
+
+  function handleCloseNewTrasactionModal() {
+      setIsNewTrasactionModalOpen(false);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTrasactionModal}/>
+
+      <Dashboard/>
+
+      <NewTransactionModal 
+      isOpen={isNewTrasactionModalOpen} 
+      onRequestClose={handleCloseNewTrasactionModal}
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
   );
 }
 
-export default App;
+
+
+
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,72 @@
+
+import entradaImg from  '../../assets/Entradas.svg';
+import saidaImg from '../../assets/Saidas.svg';
+import totalImg from '../../assets/Total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
+
+
+import { Container } from "./style";
+
+
+export function Summary( ) {
+    const {transactions} = useTransactions();
+
+ 
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    });
+
+    return(
+        <Container>
+            <div>
+            <header>
+                <p>Entradas</p>
+                <img src={entradaImg} alt="Entradas" />
+            </header>
+            <strong>
+            {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(summary.deposits)}
+            </strong>
+            </div>
+            <div>
+            <header>
+                <p>Saídas</p>
+                <img src={saidaImg} alt="Saídas" />
+            </header>
+            <strong>
+             -
+            {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(summary.withdraws)}
+            </strong>
+            </div>
+            <div className="highlight-background">
+            <header>
+                <p>Total</p>
+                <img src={totalImg} alt="Total" />
+            </header>
+            <strong>
+            {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(summary.total)}
+            </strong>
+            </div>
+        </Container>
+    )
+}
